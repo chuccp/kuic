e879246474c8d77686e6050a83b2f24d6df66257
tomcat <cooge123@gmail.com>
2023-08-28 23:12:52 +0800

    Update http_test.go

--- http/http_test.go
+++ http/http_test.go
@@ -1,38 +1,21 @@
 package http
 
 import (
-	"github.com/chuccp/kuic/cert"
-	"log"
 	"net/http"
 	"testing"
-	"time"
 )
 
 func TestServer(t *testing.T) {
 
 	keyPem := "key2.PEM"
 	certPem := "cert2.PEM"
-	server, err := createServer("0.0.0.0:2153")
-	if err != nil {
-		return
-	}
-	err = cert.CreateOrReadCert(keyPem, certPem)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("11111111111"))
 	})
+	err := ListenAndServe("0.0.0.0:6321", certPem, keyPem, serveMux)
+	if err != nil {
+		return
+	}
 
-	go func() {
-		err = server.Listen(certPem, keyPem, serveMux)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}()
-
-	time.Sleep(time.Second * 10)
 }
