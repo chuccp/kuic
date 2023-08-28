a15dd4023f445990b8f86dd980c786f95aaf23c2
tomcat <cooge123@gmail.com>
2023-08-28 23:18:27 +0800

    Update server.go

--- http/server.go
+++ http/server.go
@@ -25,7 +25,7 @@ func (server *Server) GetHttpClient() *http.Client {
 	return http3.NewClient(conn, tlsConf)
 }
 
-func (server *Server) Listen(certFile, keyFile string, handler http.Handler) error {
+func (server *Server) ListenAndServeTLS(certFile, keyFile string, handler http.Handler) error {
 	conn, err := server.baseServer.GetServerConn()
 	if err != nil {
 		return err
@@ -48,7 +48,6 @@ func (server *Server) Listen(certFile, keyFile string, handler http.Handler) err
 	qErr := make(chan error)
 	go func() {
 		hErr <- http.ListenAndServeTLS(server.addr, certFile, keyFile, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			quicServer.SetQuicHeaders(w.Header())
 			handler.ServeHTTP(w, r)
 		}))
 	}()
@@ -65,7 +64,45 @@ func (server *Server) Listen(certFile, keyFile string, handler http.Handler) err
 
 	return nil
 }
+func (server *Server) ListenAndServe(certFile, keyFile string, handler http.Handler) error {
+	conn, err := server.baseServer.GetServerConn()
+	if err != nil {
+		return err
+	}
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return err
+	}
+	config := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+	if handler == nil {
+		handler = http.DefaultServeMux
+	}
+	quicServer := &http3.Server{
+		TLSConfig: config,
+		Handler:   handler,
+	}
+	hErr := make(chan error)
+	qErr := make(chan error)
+	go func() {
+		hErr <- http.ListenAndServe(server.addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			handler.ServeHTTP(w, r)
+		}))
+	}()
+	go func() {
+		qErr <- quicServer.Serve(conn)
+	}()
+	select {
+	case err := <-hErr:
+		quicServer.Close()
+		return err
+	case err := <-qErr:
+		return err
+	}
 
+	return nil
+}
 func CreateServer(addr string) (*Server, error) {
 	server := &Server{addr: addr}
 	udpAddr, err := net.ResolveUDPAddr("udp", addr)
